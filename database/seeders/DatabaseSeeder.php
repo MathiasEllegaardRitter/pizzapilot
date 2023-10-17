ed13039cc312c6ee4611e8897948a04cbf889723
Mathias Ritter <huskmigffs@gmail.com>
2023-10-17 13:50:49 +0100

    test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,8 +21,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        Breakes::factory(8)->create();
-        Product::factory(5)->create();
+        // Breakes::factory(8)->create();
+        // Product::factory(5)->create();
 
     }
 }
