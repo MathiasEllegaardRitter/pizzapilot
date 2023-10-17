4e8e5aa4a0a40e235b364ee60289083d1a2d777c
Mathias Ritter <huskmigffs@gmail.com>
2023-10-17 13:36:23 +0100

    Added Products

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', [ProductController::class, 'readAll']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
