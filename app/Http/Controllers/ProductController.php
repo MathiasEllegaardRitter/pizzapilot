ce9e29579ac0cb907cdc632ca34032db66c3847f
Mathias Ritter <huskmigffs@gmail.com>
2023-10-17 15:54:11 +0100

    Create product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use Illuminate\Http\RedirectResponse;
 
 class ProductController extends Controller
 {
@@ -12,6 +13,36 @@ class ProductController extends Controller
     {
         $products = Product::all();
         return view('products', ['products' => $products]);
+    }
+
+    public function create(Request $request)
+    {
+
+        // Create a product and fill in the varibles with forms
+
+        $product = new Product;
+
+ 
+
+        $product->name = $request->input('name');
+
+ 
+
+        $product->price = $request->input('price');
+
+ 
+
+        $product->description = $request->input('description');
+
+        // TODO: Add ingridents
+
+
+        // Save to Database
+        $product->save();
+
+ 
+
+        return redirect()->route('products.create');
     }
 
 }
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -10,5 +10,53 @@
             <li>{{ $product->name }}</li>
         @endforeach
     </ul>
+
+    <form action="{{ route('products.create') }}" method="post">
+
+ 
+
+        @csrf
+    
+     
+    
+     
+    
+     
+    
+        <label for="name">Name:</label>
+    
+     
+    
+        <input type="text" name="name" required>
+    
+     
+    
+     
+    
+     
+    
+        <label for="price">Price:</label>
+    
+     
+    
+        <input type="number" name="price" required>
+    
+     
+    
+     
+    
+        <label for="description">Description:</label>
+        <textarea type="text" name="description" required></textarea>
+    
+       
+    
+        <button type="submit">Add Product</button>
+    
+     
+    
+    </form>
+
+
+
 </body>
 </html>
