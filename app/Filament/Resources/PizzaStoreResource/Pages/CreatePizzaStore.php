bd179db184b8df2acd8edbba5546ce801626f295
Mathias Ritter <huskmigffs@gmail.com>
2023-11-03 10:20:47 +0100

    pizzaStore created

---
+++ app/Filament/Resources/PizzaStoreResource/Pages/CreatePizzaStore.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\PizzaStoreResource\Pages;
+
+use App\Filament\Resources\PizzaStoreResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreatePizzaStore extends CreateRecord
+{
+    protected static string $resource = PizzaStoreResource::class;
+}
--- database/migrations/2023_10_16_094249_create_pizza_stores_table.php
+++ database/migrations/2023_10_16_094249_create_pizza_stores_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('pizza_stores', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string("location");
+            $table->foreignId('user_id');
         });
     }
 
