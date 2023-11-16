5127caf06fdefc45eedd021c523934d537015243
Mathias Ritter <huskmigffs@gmail.com>
2023-11-16 09:22:33 +0100

    added a indexpagesetup tailwind so it can be used

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PizzaPilot</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="h-screen w-screen bg-color">
+
+    {{-- <div class="left-0 top-0 absolute bg-gradient-to-br from-amber-500 to-neutral-800"></div> --}}
+
+
+    <h1 class="text-yellow-500">Test<h1>
+
+
+</body>
+</html>
