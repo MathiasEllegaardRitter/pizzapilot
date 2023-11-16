2d5879332785f3849c2d8f2c5c410aa78315e023
Mathias Ritter <huskmigffs@gmail.com>
2023-11-16 13:15:01 +0100

    bug fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,6 +14,7 @@
 
         <livewire:navbar />
 
+        <livewire:hero-section />
 
     </div>
 
--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -1,64 +1,63 @@
 <nav class="flex flex-row w-full h-20 items-center justify-between">
-        <img wire:click="home" class="ml-10 hover:cursor-pointer" src="{{ asset('storage/icons/PIZZAPILOT.svg') }}" alt="Icon">
+    <img wire:click="home" class="ml-10 hover:cursor-pointer" src="{{ asset('storage/icons/PIZZAPILOT.svg') }}" alt="Icon">
+    
+    <div class="text-black">
+        <input placeholder="What would you like"/>
+    </div>
+
+    <div class="flex flex-row text-orange-400 justify-between space-x-5 mr-10">
+
+    <div wire:click="cart" class="hover:cursor-pointer flex flex-col items-center">
+        <img class="w-8 h-8" src="{{ asset('storage/icons/Shopping Cart.svg') }}" alt="Icon">
+        <div>
+        Cart
+        </div>
+    </div>
+
+    <div wire:click="favorites" class="hover:cursor-pointer flex flex-col items-center">
+        <img class="w-8 h-8" src="{{ asset('storage/icons/Heart.svg') }}" alt="Icon">
+        <div>
+        Favorites
+        </div>
+    </div>
+
+    <div wire:click="message" class="hover:cursor-pointer flex flex-col items-center">
+        <img class="w-8 h-8" src="{{ asset('storage/icons/Envelope.svg') }}" alt="Icon">
+        <div>
+            Message
+        </div>
+    </div>
+
+    <div wire:click="user" class="hover:cursor-pointer flex flex-col items-center">
+        <img class="w-8 h-8" src="{{ asset('storage/icons/User.svg') }}" alt="Icon">
         
-        <div class="text-black">
-            <input placeholder="What would you like"/>
+        <div>
+        User
         </div>
 
-        <div class="flex flex-row text-orange-400 justify-between space-x-5 mr-10">
-
-        <div wire:click="cart" class="hover:cursor-pointer flex flex-col items-center">
-            <img class="w-8 h-8" src="{{ asset('storage/icons/Shopping Cart.svg') }}" alt="Icon">
+    </div>
+            @auth
             <div>
-            Cart
-            </div>
-        </div>
-
-        <div wire:click="favorites" class="hover:cursor-pointer flex flex-col items-center">
-            <img class="w-8 h-8" src="{{ asset('storage/icons/Heart.svg') }}" alt="Icon">
-            <div>
-            Favorites
-            </div>
-        </div>
-
-        <div wire:click="message" class="hover:cursor-pointer flex flex-col items-center">
-            <img class="w-8 h-8" src="{{ asset('storage/icons/Envelope.svg') }}" alt="Icon">
-            <div>
-                Message
-            </div>
-        </div>
-
-        <div wire:click="user" class="hover:cursor-pointer flex flex-col items-center">
-            <img class="w-8 h-8" src="{{ asset('storage/icons/User.svg') }}" alt="Icon">
+            {{-- <p> {{ Auth::user()->name }}!</p> --}}
+            <form action="{{ route('logout') }}" method="POST">
+            @csrf
             
-            <div>
-            User
+            
+            <div class="hover:cursor-pointer flex flex-col items-center">
+            <img class="w-8 h-8" src="{{ asset('storage/icons/Logout.svg') }}" alt="Icon">
+            <button type="submit">Logout</button>
             </div>
 
-        </div>
-                @auth
-                <div>
-                {{-- <p> {{ Auth::user()->name }}!</p> --}}
-                <form action="{{ route('logout') }}" method="POST">
-                @csrf
-                
-                
-                <div class="hover:cursor-pointer flex flex-col items-center">
-                <img class="w-8 h-8" src="{{ asset('storage/icons/Logout.svg') }}" alt="Icon">
-                <button type="submit">Logout</button>
-                </div>
+            </form>
+            </div>
+            
+            @else
 
-                </form>
-                </div>
-                
-                @else
-
-                <div class="hover:cursor-pointer flex flex-col items-center">
-                <img class="w-8 h-8" src="{{ asset('storage/icons/Enter.svg') }}" alt="Icon">
-                <div>
-                <a href="{{ route('login') }}">Login</a>
-                </div>
-                @endauth
-                </div>
-        </div>
-    </nav>
+            <div class="hover:cursor-pointer flex flex-col items-center">
+            <img class="w-8 h-8" src="{{ asset('storage/icons/Enter.svg') }}" alt="Icon">
+            <div>
+            <a href="{{ route('login') }}">Login</a>
+            </div>
+            @endauth
+            </div>
+</nav>
