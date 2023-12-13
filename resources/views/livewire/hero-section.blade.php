3d1b1dd9f41e74d5bc33979e6369798011bf3e88
Emil-J1 <10407718@ucn.dk>
2023-12-13 09:36:52 +0100

    Hero section done

--- resources/views/livewire/hero-section.blade.php
+++ resources/views/livewire/hero-section.blade.php
@@ -1,4 +1,4 @@
-<div class="flex flex-row items-center content-center h-full w-full 2xl:mt-60 mt-40 mb-16">
+<div class="flex flex-row items-center content-center h-full w-full 2xl:mt-80 mt-40 mb-16">
 
   <div class="flex items-center justify-end text-white w-1/3 hero-section-font font-bold">
       Every bite a
